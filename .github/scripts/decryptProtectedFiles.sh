43c3338e83305df098d230a8c854dde5cf789826
Craig Gregory <craig.jonathan.gregory@gmail.com>
2022-06-14 12:49:34 -0500

    Decrypt .protected files #16

--- .github/scripts/decryptProtectedFiles.sh
+++ .github/scripts/decryptProtectedFiles.sh
@@ -3,12 +3,11 @@
 # Recursively travel a directory and decrypt files
 
 function traverse() {
-    for src in "$1"/*
+    for src in ".protected/.protected"/*
     do
         if [ ! -d "${src}" ] ; then
             echo "${src} is a file"
-            echo "${#1}+1"
-            dst=${src:${#1}+1:-4}
+            dst=${src:22:-4}
             echo "${dst}"
             # gpg --decrypt --passphrase "$(1)" --batch -o "${dst}" "${src}"
         else
